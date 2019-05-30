21fb0ed5f67f6523ccaa78171727c47cf3da2aa0
fenzizichan(ios) <528367007@qq.com>
2019-05-30 10:56:47 +0800

    Initial Commit

---
+++ ReactiveObjcDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactiveObjcDemo
+//
+//  Created by 王龙飞 on 2019/5/30.
+//  Copyright © 2019年 MDDPersonal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
